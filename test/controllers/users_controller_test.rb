706de55d886e728edd99ad2e3ca4f320e614cabe
Tsuyoshi Tomozawa <sphdm244@gmail.com>
2022-08-01 00:06:31 +0900

    feat: Add test for UserController#show

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
+    include Warden::Test::Helpers
+
+    def setup
+        @user = users(:michael)
+        login_as @user
+    end
+
+    test "should get show" do
+        get user_url @user
+        assert_response :success
+        assert_template 'users/show'
+    end
 end
